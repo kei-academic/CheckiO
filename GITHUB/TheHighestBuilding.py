4f0a721312229b70709ff920c190809febcb8bb9
keigo_shimada <e215730@ie.u-ryukyu.ac.jp>
2021-12-15 16:30:03 +0900

    :+1: `Add` TheHighestBuilding.py

---
+++ GITHUB/TheHighestBuilding.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+from typing import List
+
+def highest_building(buildings: List[List[int]]) -> List[int]:
+    heights = [0] * len(buildings[0])
+    for i in range(len(buildings)):
+        for j in range(len(buildings[0])):
+            heights[j] += buildings[i][j]
+    ans = [heights.index(max(heights)) + 1, max(heights)]
+    return ans
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(highest_building([
+        [0, 0, 1, 0],
+        [1, 0, 1, 0],
+        [1, 1, 1, 0],
+        [1, 1, 1, 1]
+    ]))
+
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert highest_building([
+        [0, 0, 1, 0],
+        [1, 0, 1, 0],
+        [1, 1, 1, 0],
+        [1, 1, 1, 1]
+    ]) == [3, 4], "Common"
+    assert highest_building([
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 1]
+    ]) == [4, 1], "Cabin in the wood"
+    assert highest_building([
+        [1, 0, 0, 0, 0],
+        [1, 1, 0, 0, 0],
+        [1, 1, 1, 0, 0],
+        [1, 1, 1, 1, 0],
+        [1, 1, 1, 1, 1]
+    ]) == [1, 5], "Triangle"
+    assert highest_building([
+        [0, 0, 0, 1, 0, 0, 0],
+        [0, 0, 1, 1, 1, 0, 0],
+        [0, 1, 1, 1, 1, 1, 0],
+        [1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1]
+    ]) == [4, 6], "Pyramid"
+    print("Coding complete? Click 'Check' to earn cool rewards!")
